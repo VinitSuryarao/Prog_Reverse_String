fed886f6e95cbb5f8059800aac60c64a47bb501f
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:02:33 +0530

    Add files via upload

---
+++ Reverse_String.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //   Reverse String
+        //   Vinit = tinitV
+        static void Main(string[] args)
+        {
+            
+            string str = "Vinit", revstr=null;
+            
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                revstr = revstr + str[i];
+            }
+
+            Console.WriteLine(revstr);
+            Console.ReadLine();
+        }
+    }
+
+
